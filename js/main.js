6f70a88de2b9b26ed5c202342437ab0aa082a95c
Rohit Kumar Gautam <nirmal.rohit@yahoo.co.in>
2017-07-29 00:50:19 +0530

    add click in navbar

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
 (function ($, win) {
 	var header = $('#rs-header');
-	
+	var winHeight = 0;
+			
 	var setHeight = function() {
-		var winHeight = $( window ).height();
+		winHeight = $( window ).height();
 
 		$('#rs-hero').css({
 			height: winHeight - 64
 		});
 	};
+	
+	var setminHeight = function () {
+		$('[data-min-height]').each(function() {
+			$(this).css({
+				'min-height': winHeight
+			})
+		})
+	};
 
 	setHeight();
+	setminHeight();
+	
+	header.on('click', 'a', function(){
+		var id = $(this).data('section');
+		
+		$('html,body').animate({
+			scrollTop: $(id).offset().top
+		}, 'slow');
+	});
 	
 	$(win).on('resize', function(){
 		setHeight();
+		setminHeight();
 	});
 	
 })(jQuery, window);
