198da6fa569358e07432b757258c6563e29a586f
github-rohit <nirmal.rohit@yahoo.co.in>
2018-05-09 22:09:32 +0530

    Add service worker for caches managment

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 (function ($, win) {
+    'use strict';
+    
 	var header = $('#rs-header');
 	var winHeight = 0;
 			
 	var setHeight = function() {
-		winHeight = $( window ).height();
+		winHeight = $(win).height();
 
 		$('#rs-hero').css({
 			height: winHeight - 64
@@ -14,14 +16,14 @@
 		$('[data-min-height]').each(function() {
 			$(this).css({
 				'min-height': winHeight
-			})
-		})
+			});
+		});
 	};
 
 	setHeight();
 	setminHeight();
 	
-	header.on('click', 'a', function(){
+	header.on('click', 'a', function() {
 		var id = $(this).data('section');
 		
 		$('html,body').animate({
@@ -29,9 +31,14 @@
 		}, 'slow');
 	});
 	
-	$(win).on('resize', function(){
+	$(win).on('resize', function() {
 		setHeight();
 		setminHeight();
 	});
+    
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js');
+    }
+    
 	
 })(jQuery, window);
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_STATIC_NAME = 'static';
+const CACHE_DYNAMIC_NAME = 'dynamic';
+
+self.addEventListener('install', function(event) {
+    console.log('[Servies worker]', event);
+    
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME).then((cache) => {
+            cache.addAll([
+                '/manifest.json',
+                '/',
+                'index.html',
+                'img/favicon.png',
+                '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+                '/css/font-awesome.min.css',
+                '/css/main.css',
+                '//code.jquery.com/jquery-3.1.0.js',
+                '/js/main.js',
+            ]);
+        }).catch((err) => {
+            console.log(err);
+        })
+    );
+    
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('[Servies Activate]', event);
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(keyList.map(key => {
+                if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                    return caches.delete(key);
+                }
+            }))
+        }).catch(err => {
+            console.log(err);
+        })
+    );   
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request).then(res => {
+                    return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+                        cache.put(event.request.url, res.clone());
+                        return res;
+                    })
+                }).catch(err => {
+                    console.log(err);
+                });
+            }
+        })
+    )
+});
