3d99522a732cac2ed972aa5efc8da966ca546885
github-rohit <nirmal.rohit@yahoo.co.in>
2018-05-29 16:00:50 +0530

    service worker update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './services/data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { Index1Component } from './index-1/index-1.component';
 import { Index2Component } from './index-2/index-2.component';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { Index2Component } from './index-2/index-2.component';
     Index2Component
   ],
   imports: [
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([{
