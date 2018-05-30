20e0173cb0d0ae38b10d6a0c9b37b1a3a6082956
github-rohit <nirmal.rohit@yahoo.co.in>
2018-05-31 00:23:49 +0530

    Auto-generated commit

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ workbox.clientsClaim();
 //   new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'),
 //   workbox.strategies.staleWhileRevalidate()
 // );
-
+workbox.routing.registerNavigationRoute('index.html');
 workbox.routing.registerRoute(
     new RegExp('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'),
     workbox.strategies.cacheFirst({
